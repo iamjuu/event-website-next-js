9c780aef7285c095ff34e453140ae70a5b10291d
Fayiz kv <fayizkvcklm2002@gmail.com>
2025-03-13 15:22:48 +0530

    fetched details"

--- app/page.tsx
+++ app/page.tsx
@@ -19,63 +19,78 @@ import { eventService, sessionService, useEvent } from "./api/api";
 import { BlackImage } from "@/public";
 
 const Index = () => {
-  const { eventId, error, loading } = useEvent();
+  // const { eventId, error, loading } = useEvent();/
   const [eventData, setEventData] = useState();
-  // useEffect(() => {
-  //   const fetchEventData = async () => {
-  //     if (!eventId) {
-  //       console.log("No event ID available");
-  //       return;
-  //     }
 
-  //     try {
-  //       const data = await eventService.getEventDetails(eventId);
-  //       console.log("Event details data:", data);
-  //     } catch (error) {
-  //       console.error("Error fetching event details:", error);
-  //     }
-  //   };
-
-  //   fetchEventData();
-  // }, [eventId]);
-
-  useEffect(()=>{
-    const fetchEventData = async ()=>{
-    const response = await fetch(
-      // `${API_ENDPOINTS.EVENT}?domain=${window.location.hostname}`
-      `https://backend-endpoint.eventhex.ai/api/v1/auth/domain-event?domain=my-event.eventhex.ai`);
-      const data = await response.json()
-      console.log("Data", data);
-      setEventData(data?.domainData?.domain);
-    }
-  
-    fetchEventData();
-  },[]);
-
-  if (loading) {
-
-
-    return <div>Loading...</div>;
-  }
-
-  if (error || !eventData) {
-    return <div>Error: {error}</div>;
-  }
 
   const ticketSectionRef = useRef<HTMLDivElement>(null);
   const registerCardRef = useRef<HTMLDivElement>(null);
   const contentSectionRef = useRef<HTMLDivElement>(null);
   const ticketHeadingRef = useRef<HTMLDivElement>(null);
+
+  const BASE_URL = 'https://instarecap-app.ambitiousforest-1ab41110.centralindia.azurecontainerapps.io/api';
+  const BACKEND_URL = 'https://backend-endpoint.eventhex.ai'
+  
+  useEffect(()=>{
+    const fetchDetails = async ()=>{
+      const response = await fetch(
+        // `https://backend-endpoint.eventhex.ai/api/v1/auth/domain-event?domain=${window.location.hostname}`
+        `${BACKEND_URL}/api/v1/auth/domain-event?domain=my-event.eventhex.ai`
+        
+      );
+      const data = await response.json();
+      setEventData(data.domainData.event);
+      console.log("data", data);
+    }
+    fetchDetails();
+  },[])
+
+  // useEffect(()=>{
+
+  //   const fetchDetails = async ()=>{
+  //     const response = await fetch (`${BASE_URL}/sessions/event?eventId=${eventData._id}`);
+  //     const data = await response.json();
+  //     console.log("Domain", data);
+  //   }
+
+  //   if ( ! eventData?._id ) return ;
+  //   fetchDetails();
+  // },[eventData?._id])
+
+  useEffect(()=>{
+    const fetchDetails = async ()=>{
+      const response = await fetch(
+        // `https://backend-endpoint.eventhex.ai/api/v1/auth/domain-event?domain=${window.location.hostname}`
+        `${BACKEND_URL}/api/v1/ticket?event=${eventData?._id}`
+      );
+      const data = await response.json();
+      console.log("data", data);
+    }
+    if ( !eventData?._id) return;
+    fetchDetails();
+  },[eventData?._id])
+
+
+
+  
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error || !eventData) {
+  //   return <div>Error: {error}</div>;
+  // }
+
   
   return (
     <div className="bg-white flex flex-col items-stretch pt-4 md:pt-[26px] pb-28 md:pb-[70px]">
-      <Header logo={eventData.logo} title={eventData.title} />
+      <Header logo={eventData?.logo} title={eventData?.title} />
       <main className="self-center flex w-full max-w-[1208px] flex-col items-stretch px-4">
         <Hero 
-          title={eventData.title}
-          banner={eventData.banner}
-          startDate={eventData.startDate}
-          venue={eventData.venue}
+          title={eventData?.title}
+          banner={eventData?.banner}
+          startDate={eventData?.startDate}
+          venue={eventData?.venue}
         />
         
         <div className="mt-6 md:mt-[25px] max-md:max-w-full">
@@ -83,11 +98,11 @@ const Index = () => {
             <div className="w-full lg:w-[64%] max-md:w-full max-md:ml-0">
               <div className="flex grow flex-col items-stretch max-md:max-w-full max-md:mt-6">
                 <EventDetails 
-                  startDate={eventData.startDate}
-                  endDate={eventData.endDate}
-                  venue={eventData.venue}
+                  startDate={eventData?.startDate}
+                  endDate={eventData?.endDate}
+                  venue={eventData?.venue}
                 />
-                <AboutEvent description={eventData.description} />
+                <AboutEvent description={eventData?.description} />
                 <KeyFeatures />
               </div>
             </div>
